a645e1d626f1d30b9a2733200b2b7e8db0f3f7f6
davidfq <davidfq@gmail.com>
2024-10-29 18:44:40 +0100

    Update example

--- examples/create_psoxy_connections/main.tf
+++ examples/create_psoxy_connections/main.tf
@@ -17,6 +17,9 @@ module "create_psoxy_connection_script" {
   psoxy_connection = {
     integration = each.value.integration
     endpoint    = each.value.endpoint
+    bucket      = each.value.bucket
+    parser_id   = each.value.parser_id
+    github_organization = each.value.github_organization
   }
   psoxy_connection_script_path = coalesce(var.psoxy_connection_script_path, path.module)
   service_account_email        = module.tenant_api_auth.worklytics_tenant_api_sa
