f56e9c24cfb8328846f8b304aedf68eba6cb0a34
davidfq <davidfq@gmail.com>
2024-10-29 18:44:27 +0100

    Improve script generation
    Use jsonencode per docs advice:
    https://developer.hashicorp.com/terraform/language/expressions/strings#generating-json-or-yaml

--- modules/psoxy_connection/main.tf
+++ modules/psoxy_connection/main.tf
@@ -1,3 +1,17 @@
+locals {
+  settings = merge(
+    { PROXY_DEPLOYMENT_KIND = "GCP" },
+    var.psoxy_connection.bucket != null ? { PROXY_BUCKET = var.psoxy_connection.bucket } : {},
+    var.psoxy_connection.endpoint != null ? { PROXY_ENDPOINT = var.psoxy_connection.endpoint } : {},
+    var.psoxy_connection.parser_id != null ? { parserId = var.psoxy_connection.parser_id } : {},
+    var.psoxy_connection.github_organization != null ? { GITHUB_ORGANIZATION = var.psoxy_connection.github_organization } : {}
+  )
+  json_payload = jsonencode({
+    integrationId = var.psoxy_connection.integration,
+    settings = local.settings
+  })
+}
+
 resource "local_file" "worklytics_tenant_api_script_file" {
   content         = <<EOT
 #!/bin/bash
@@ -24,13 +38,7 @@ curl -X POST https://${var.tenant_api_host}/tenant-api/data-connections \
   -H "Authorization: Bearer $TOKEN" \
   -H "x-worklytics-tenant-id: ${var.worklytics_tenant_id}"\
   -H "Content-Type: application/json" \
-  -d '{
-       "integrationId": "${var.psoxy_connection.integration}",
-        "settings": {
-          "PROXY_DEPLOYMENT_KIND": "GCP",
-          "PROXY_ENDPOINT": "${var.psoxy_connection.endpoint}"
-        }
-     }'
+  -d "${local.json_payload}"
 EOT
   filename        = "${coalesce(var.psoxy_connection_script_path, path.module)}/create_${var.psoxy_connection.integration}_connection.sh"
   file_permission = "0755"
